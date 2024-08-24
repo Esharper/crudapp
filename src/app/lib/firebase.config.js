451bc96dc1d908a3e7c413b8be12d915e133860f
esharper87 <Elginsharper87@gmail.com>
2024-08-24 01:48:09 -0500

    init

--- src/app/lib/firebase.config.js
+++ src/app/lib/firebase.config.js
@@ -1,23 +1,23 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCZccYb_8M9VyuSHRGnr08jNUxaoeTrz5o",
-//   authDomain: "complete-welder-427123-c6.firebaseapp.com",
-//   projectId: "complete-welder-427123-c6",
-//   storageBucket: "complete-welder-427123-c6.appspot.com",
-//   messagingSenderId: "220769308710",
-//   appId: "1:220769308710:web:5432f7eb0f28c6a3bc96e7",
-//   measurementId: "G-C1Z6HBLYN2"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZccYb_8M9VyuSHRGnr08jNUxaoeTrz5o",
+  authDomain: "complete-welder-427123-c6.firebaseapp.com",
+  projectId: "complete-welder-427123-c6",
+  storageBucket: "complete-welder-427123-c6.appspot.com",
+  messagingSenderId: "220769308710",
+  appId: "1:220769308710:web:5432f7eb0f28c6a3bc96e7",
+  measurementId: "G-C1Z6HBLYN2"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-// const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
 
-// export { app, analytics };
+export { app, auth, firestore, analytics };
--- src/app/management/page.js
+++ src/app/management/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { Auth } from "../lib/firebase.config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
 
